69cbc5dc13925e5e4300f41dd303cc4e78be8c05
Ola <ola.dobronf@gmail.com>
2020-08-14 15:25:06 -0400

    Code refactor, added filter options and default loading database

--- Python3 practice/books.py
+++ Python3 practice/books.py
@@ -1,3 +1,6 @@
+import os.path
+
+
 class Book:
     def __init__(self, title, authors, rating, read):  # constructor - object initialisation
         self.title = title  # self - reference to created object - new declarations container
@@ -27,17 +30,21 @@ class BookBot:
     def __init__(self):
         self.shelf = []
 
+# -----=------ Bot interaction functionalities -----=------
+
     def introduce(self):
         print("\nHi, Im bot Olivier - I will assist you. Pick what you want to do: ")
+        self.load_books()
         self.help()
         self.options()
-        self.load_books()
 
     def help(self):
         print("\n Commands: ")
         print("Add - Add new book")
         print("List - List books from shelf")
         print("Modify - Choose a book and change record")
+        print("View rating - filter shelf by books rating from  defined scope")
+        print("View read - filter shelf by read books")
         print("Save - Save database to csv file")
         print("Help - View available commands")
         print("Exit - Say goodbye to Olivier")
@@ -50,13 +57,17 @@ class BookBot:
             self.list_books()
         elif text == "Modify":
             self.pick_book_change()
+        elif text == "View rating":
+            self.filter_rating()
+        elif text == "View read":
+            self.filter_read()
         elif text == "Save":
             self.save_books()
-        elif text == "Exit":
-            exit()
         elif text == "Help":
             self.help()
             self.options()
+        elif text == "Exit":
+            exit()
         else:
             self.repeat()
 
@@ -64,23 +75,15 @@ class BookBot:
         print("Unrecognised operation, try again")
         self.options()
 
+# -----=------ Adding items to list -----=------
+
     def add_book(self, title, authors, rating, read):
         b = Book(title, authors, rating, read)  # create object Book
         self.shelf.append(b)  # add to list
 
-    def print_book(self, index):
-        book = self.shelf[index]
-        print("{:4} | {:30} | {:30} | {:5} | {:5}".format(index, book.title, book.authors, book.rating, book.read))
-
-    def list_books(self):
-        for index in range(len(self.shelf)):
-            self.print_book(index)
-        print("\n---- ----- ---- ----- ---- ----")
-        self.options()
-
-    def insert_book(self):
-        self.add_book("Pan Tadeusz", "Adam Mickiewicz", 1, False)
-        self.add_book("Splatana Siec", "Michal Zalewski", 10, True)
+   # def insert_book(self):
+   #     self.add_book("Pan Tadeusz", "Adam Mickiewicz", 1, False)
+   #     self.add_book("Splatana Siec", "Michal Zalewski", 10, True)
 
     def input_add_book(self):
         title = input("Title: ")
@@ -89,8 +92,10 @@ class BookBot:
         read = input("Read? ")
 
         self.add_book(title, author, rating, read)
-        print("\n---- ----- ---- ----- ---- ----")
+        print("--------------------------")
         self.options()
+
+# -----=------ Modify items in list -----=------
 
     def pick_book_change(self):
         print("\nPick up book what you want to change!")
@@ -115,20 +120,66 @@ class BookBot:
             book.set_read(input("Insert new read status True/False: "))
         else:
             print("Unknown value - try again")
-        print("\n---- ----- ---- ----- ---- ----")
+        print("--------------------------")
         self.options()
 
-    def print_all_read_book(self):
+# -----=------ Print books -----=------
+
+    def print_book(self, index):
+        book = self.shelf[index]
+        print("{:4} | {:30} | {:30} | {:5} | {:5}".format(index, book.title, book.authors, book.rating, book.read))
+
+    def list_books(self):
+        print("\n--------------------------")
+        for index in range(len(self.shelf)):
+            self.print_book(index)
+        print("--------------------------")
+        self.options()
+
+    def print_all_read_book(self, choice):
+        print("\n--------------------------")
         for i in range(len(self.shelf)):
             book = self.shelf[i]
-            if book.read:
-                self.print_book(i)
+            if choice == 1:
+                if book.read:
+                    self.print_book(i)
+            else:
+                if not book.read:
+                    self.print_book(i)
+        print("--------------------------")
 
     def print_all_rating_books(self, min, max):
+        print("\n--------------------------")
         for i in range(len(self.shelf)):
             book = self.shelf[i]
             if min <= book.rating <= max:
                 self.print_book(i)
+        print("--------------------------")
+
+# -----=------ Filtering options -----=------
+
+    def filter_rating(self):
+        print("Rating value scope - 1.0 - 10.0 \nUnset value - 0")
+        min = float(input("Min rating value: "))
+        max = float(input("Max rating value: "))
+        if min >= 1.0 and max <= 10.0:
+            self.print_all_rating_books(min, max)
+            self.options()
+        else:
+            print("Improper rating value - try again")
+            self.options()
+
+    def filter_read(self):
+        print("Sort by: \n read - 1 \n unread - 0")
+        choice = int(input("\n Choose (1 or 0): "))
+        if 0 >= choice or choice <= 1:
+            self.print_all_read_book(choice)
+            self.options()
+        else:
+            print("Improper read value - try again")
+        self.options()
+
+# -----=------ Load/Save items in list (file) -----=------
 
     def save_books(self):
         text_holder = []
@@ -142,17 +193,18 @@ class BookBot:
         self.options()
 
     def load_books(self):
-        with open('books.csv', 'r') as f:
-            for line in f:
-                book_list = line.split(';')
-                if book_list[3] == 'True\n':
-                    read = True
-                else:
-                    read = False
-                self.add_book(book_list[0], book_list[1], float(book_list[2]), read)
+        if os.path.isfile('./books.csv'):
+            with open('books.csv', 'r') as f:
+                for line in f:
+                    book_list = line.split(';')
+                    if book_list[3] == 'True\n':
+                        read = True
+                    else:
+                        read = False
+                    self.add_book(book_list[0], book_list[1], float(book_list[2]), read)
+            print("\nDatabase loaded!\n")
 
 
 bot = BookBot()
-bot.insert_book()
-#bot.introduce()
+bot.introduce()
 
